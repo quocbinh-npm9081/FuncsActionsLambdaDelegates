6b0388c511508ab79a1a85c16ad09d2eb94e4d93
quocbinhgithub@gmail.com <quocbinhgithub@gmail.com>
2026-01-04 10:07:06 +0700

    cache

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+IDataDownloader dataDownloader = new CachingDataDownloader(new SlowDataDownloader());
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+
+public class Cache<TKey, TData> where TKey : notnull
+{
+    private readonly Dictionary<TKey, TData> _cacheData = new();
+
+    public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime)
+    {
+        if (!_cacheData.ContainsKey(key))
+        {
+            _cacheData[key] = getForTheFirstTime(key);
+        }
+
+        return _cacheData[key];
+    }
+}
+
+
+public interface IDataDownloader
+{
+    string DownloadData(string resourceId);
+}
+
+public class CachingDataDownloader : IDataDownloader
+{
+    private readonly IDataDownloader _dataDownloader;
+    private readonly Cache<string, string> _cache = new();
+
+    public CachingDataDownloader(IDataDownloader  dataDownloader)
+    {
+        _dataDownloader =  dataDownloader;
+    }
+    public string DownloadData(string resourceId)
+    {
+        return _cache.Get(resourceId, _dataDownloader.DownloadData);
+    }
+}
+
+public class SlowDataDownloader : IDataDownloader
+{
+    public string DownloadData(string resourceId)
+    {
+        Thread.Sleep(3000);
+        return $"Some data for {resourceId}";
+    }
+  
+}
